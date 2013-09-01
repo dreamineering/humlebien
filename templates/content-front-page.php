bf213d9d31484e375686ec5da2e5ff64a73d5c3f
Matt Mischewski <matt@dreamineering.com>
2013-09-01 21:23:03 +0200

    added events to front page;

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -28,6 +28,63 @@
   <div class="grid">
     <div class="grid__item one-whole desk-one-half">
       <h2>Coming Events</h2>
+
+        <?php
+
+    $the_query = new WP_Query(array(
+      'post_type' => 'events',
+      'posts_per_page' => 3,
+      'orderby'   => 'menu_order'
+    ));
+
+    while ($the_query->have_posts()) :
+      $the_query->the_post();
+    ?>
+
+    <article class="post event" id="post-<?php the_ID(); ?>">
+
+      <time>
+        <?php the_field('date_of_event'); ?>
+      </time>
+
+      <h2>
+
+        <?php if (get_field('event_link')) { ?>
+
+          <a href="<?php the_field('event_link'); ?>">
+            <?php the_title(); ?>
+          </a>
+
+        <?php } else {  ?>
+
+          <?php the_title(); ?>
+
+        <?php } ?>
+
+      </h2>
+
+      <dl>
+
+        <dt>Hvor?</dt>
+        <dd>
+          <?php the_field('venue'); ?>
+        </dd>
+          
+        <dt>Hvem?</dt>
+        <dd>
+          <?php if (get_field('participants')) { ?>
+            <?php the_field('participants'); ?>
+          <?php } ?>
+        </dd>
+
+
+      </dl>
+
+    </article>
+
+  <?php endwhile; ?>
+
+
     </div><!--
   --><div class="grid__item one-whole desk-one-half">
       <h2>Latest Buzz</h2>
