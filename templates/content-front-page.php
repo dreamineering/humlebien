b3395d4293f17a1188f90a57abff7e925963aab9
Matt Mischewski <matt@dreamineering.com>
2013-09-01 16:48:17 +0200

    more styling for landing page

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -4,10 +4,36 @@
       <?php the_content(); ?> 
     </div><!--
  --><div class="grid__item one-whole desk-one-third">
-      <img src="assets/img/honeybee.png" alt="">
+      <div class="logo">
+        <img src="assets/img/honeybee.png" class="logo__img" alt="">  
+      </div>
     </div>
-
-
   </div>
+
+  <div class="island island--cta">
+    <div class="grid">
+      <div class="grid__item one-whole desk-one-half">
+        <h3>
+          Har vi plads i bikuben for dine børn.
+        </h3>
+      </div><!--
+    --><div class="grid__item one-whole desk-one-half">
+        <a class="btn btn--cta">Join waiting list</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="island island--latest">
+    <div class="grid">
+      <div class="grid__item one-whole desk-one-half">
+        <h2>Coming Events</h2>
+      </div><!--
+    --><div class="grid__item one-whole desk-one-half">
+        <h2>Latest Buzz</h2>
+      </div>
+    </div>
+  </div>
+
+
   <?php wp_link_pages(array('before' => '<nav class="nav pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
