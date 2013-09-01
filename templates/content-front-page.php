624d1ed3ae3626068e00e6b0485ab53339e25570
Matt Mischewski <matt@dreamineering.com>
2013-09-01 10:01:46 +0200

    added bee

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -1,4 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
-  <?php the_content(); ?>
+  <div class="grid">
+    <div class="grid__item one-whole desk-one-third">
+      <img src="assets/img/honeybee.png" alt="">
+    </div><!--
+    --><div class="grid__item one-whole desk-two-thirds">
+      <?php the_content(); ?> 
+    </div>
+
+  </div>
   <?php wp_link_pages(array('before' => '<nav class="nav pagination">', 'after' => '</nav>')); ?>
 <?php endwhile; ?>
